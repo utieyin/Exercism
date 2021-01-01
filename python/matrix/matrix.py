cc781e673fd16dad175ccabacc2ee6eb71e37e29
utieyin <uti1@hotmail.co.uk>
2021-01-01 22:31:02 +0000

    refactor matrix

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,41 +1,31 @@
 class Matrix:
     def __init__(self, matrix_string):
-        value = matrix_string.split("\n")
-        self.value =value
+        result = []
+        for k in matrix_string.split("\n"):
+            k = [int(x) for x in k.split()]
+            result.append(k)
+        self.result =result
 
     def row(self, index):
-        result = {}
-        for i, k in enumerate(self.value):
-            k = k.strip()
-            result[i] = k
-        pos = index -1
-
-        if pos in result:
-            res = [int(x) for x in result[pos].split()]
-            return res
+        pos=index-1
+        if self.result[pos]:
+            return self.result[pos]
         else:
             raise Exception("Key not present in dict")
 
     def column(self, index):
         pos = index-1
-        result = []
         a = 0
         p = 0
         t = {}
-        for k in self.value:
-            k = [int(x) for x in k.split()]
-            result.append(k)
-
-        for k in range(len(result[p])):
-            while a < len(result[p]):
+        for k in range(len(self.result[p])):
+            while a < len(self.result[p]):
                 row = []
-                for f in range(len(result)):
-                    row.append(result[f][a])
+                for f in range(len(self.result)):
+                    row.append(self.result[f][a])
                 t[a] = row
                 a += 1
-                break;
         if pos in t:
             return t[pos]
         else:
             raise Exception("Key not in dict")
-
