4d24cac7c068d4c51b12a5d30e0ce7ab20778721
utieyin <uti1@hotmail.co.uk>
2021-01-22 20:53:44 +0000

    a new version of raindrop

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,4 +1,4 @@
 def convert(number):
     drops = {3:"Pling", 5:"Plang", 7:"Plong"}
-    result ="".join([drops[x] for x in range(1,8) if x in drops and (number%x==0)])
+    result="".join([values for key, values in drops.items() if number%key==0])
     return(result if result else str(number))
