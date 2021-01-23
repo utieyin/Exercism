e41af2509083aef0fee6a3f5b9b7a03b9f230fc3
utieyin <uti1@hotmail.co.uk>
2021-01-23 00:59:22 +0000

    Updated luhn

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,17 +1,16 @@
 class Luhn:
     def __init__(self, card_num):
-        self.card_number = [card for card in card_num.replace(" ", "")]
+        self.card_number = card_num.replace(" ", "")
 
     def valid(self):
+        if not self.card_number.isdigit():
+            return False
         if len(self.card_number) > 1:
-            for number in self.card_number:
-                if not number.isnumeric():
-                    return False
             cards = [int(x) for x in self.card_number]
-            a = 2
-            while a <= len(cards):
-                pos = len(cards) - a
+            even = 2
+            while even <= len(cards):
+                pos = len(cards) - even
                 cards[pos] = (cards[pos] * 2) - 9 if cards[pos] * 2 > 9 else cards[pos] * 2
-                a += 2
+                even += 2
             return True if sum(cards) % 10 == 0 else False
         return False
