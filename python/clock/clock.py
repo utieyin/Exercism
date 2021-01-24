b7516e715d10fd5af8300036c8944c488a9db85e
utieyin <uti1@hotmail.co.uk>
2021-01-24 11:47:56 +0000

    Completed clock exercise

---
+++ python/clock/clock.py
@@ -0,0 +1,28 @@
+import time
+import datetime
+
+
+class Clock:
+    def __init__(self, hour, minute):
+        self.hour = hour
+        self.minute = minute
+
+    def __repr__(self):
+        n = (self.hour * 3600) + (self.minute * 60)
+        t = time.strftime("%H:%M", time.gmtime(n))
+        return t
+
+    def __eq__(self, other):
+        return (self.__repr__() == other.__repr__())
+
+    def __add__(self, minutes):
+        return Clock(self.hour, self.minute + minutes)
+
+    def __sub__(self, minutes):
+        return Clock(self.hour, self.minute - minutes)
+
+
+# print(Clock(201, 3001))
+# Clock(10, 0) + 3
+# Clock(10, 0) - 3
+# Clock(3, 11), Clock(99, 11)
